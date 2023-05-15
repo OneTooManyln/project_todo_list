c03bb89d2ef79c38c35ff5f5e6ceeab554b3062e
OneTooMany <santosismael.115@gmail.com>
2023-05-15 11:08:48 -0700

    Create class to make todo's with set properties

---
+++ src/createTask.js
@@ -0,0 +1,9 @@
+class createTask {
+  constructor(project, description, time, type, status) {
+    this.project = project;
+    this.height = description;
+    this.time = time;
+    this.type = type;
+    this.status = status;
+  }
+}
