d86c827b54d64adc6ca74f0a684da0411629cf92
OneTooMany <santosismael.115@gmail.com>
2023-05-15 13:18:08 -0700

    Export function to be used to create new task

--- src/createDOM.js
+++ src/createDOM.js
@@ -1,1 +1,9 @@
+const createNewTask = function () {
+  const mainContent = document.querySelector(".main-content");
 
+  const newTask = document.createElement("div");
+  newTask.classList.add("todo-item");
+  mainContent.appendChild(newTask);
+};
+
+export default createNewTask;
