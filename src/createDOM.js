6b44bc7aca3eb3c7270fbd1e9845906b9daa92b0
OneTooMany <santosismael.115@gmail.com>
2023-05-17 15:45:52 -0700

    Add rest of default classes to newTask

--- src/createDOM.js
+++ src/createDOM.js
@@ -4,6 +4,21 @@ const createNewTask = function () {
   const newTask = document.createElement("div");
   newTask.classList.add("todo-item");
   mainContent.appendChild(newTask);
+
+  const checkItem = document.createElement("div");
+  checkItem.classList.add("check-item");
+  newTask.appendChild(checkItem);
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("text-container");
+  newTask.appendChild(textContainer);
+
+  const titleText = document.createElement("p");
+  textContainer.appendChild(titleText);
+
+  const date = document.createElement("div");
+  date.classList.add("time");
+  textContainer.appendChild(date);
 };
 
 export default createNewTask;
