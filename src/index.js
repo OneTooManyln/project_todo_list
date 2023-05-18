fb8865e0d01599c278f04eb8370eff61f9153758
OneTooMany <santosismael.115@gmail.com>
2023-05-17 21:45:29 -0700

    Change name of imported function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import createNewTask from "./createDOM";
+import createNewDOMTask from "./createDOM";
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  createNewTask();
+  createNewDOMTask();
 });
