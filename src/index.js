3572d52959f8dc969612328bd38ef41e3cdec9b8
OneTooMany <santosismael.115@gmail.com>
2023-05-18 17:57:33 -0700

    Create new task class with form inputs
    Upon submit the new task will be pushed to the array to be stored.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import createNewDOMTask from "./createDOM";
+import task from "./createTask";
+import { taskArray } from "./createTask";
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  createNewDOMTask();
+  const titleInput = document.querySelector("#task-title");
+  const descriptionInput = document.querySelector("#task-description");
+
+  const newTask = new task(titleInput.value, descriptionInput.value);
+  taskArray.push(newTask);
+  console.table(taskArray);
 });
