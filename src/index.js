30267d493c0f390f33bd8a2ffd0d673c6bfb93f3
OneTooMany <santosismael.115@gmail.com>
2023-05-19 14:53:50 -0700

    Create function to cleare HTML elements

--- src/index.js
+++ src/index.js
@@ -10,10 +10,20 @@ import { default as task, taskArray } from "./createTask";
 const form = document.querySelector("form");
 const todoItem = document.querySelectorAll(".todo-item");
 const newTaskBtn = document.querySelector(".new-task");
+const mainContent = document.querySelector(".main-content");
+
+createArrayTask(taskArray);
+
+const clearLists = function () {
+  while (mainContent.firstChild) {
+    mainContent.removeChild(mainContent.firstChild);
+  }
+};
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   hideTaskForm();
+  clearLists();
 
   const titleInput = document.querySelector("#task-title");
   const descriptionInput = document.querySelector("#task-description");
