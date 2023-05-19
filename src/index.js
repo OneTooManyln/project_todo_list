cfab6f2fe715afb45cdfd26217353f44a71325eb
OneTooMany <santosismael.115@gmail.com>
2023-05-18 18:49:10 -0700

    Call dom function with form inputs

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import { taskArray } from "./createTask";
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const titleInput = document.querySelector("#task-title");
   const descriptionInput = document.querySelector("#task-description");
 
   const newTask = new task(titleInput.value, descriptionInput.value);
   taskArray.push(newTask);
   console.table(taskArray);
+
+  createNewDOMTask(titleInput.value, descriptionInput.value);
 });
