6057aa1b9238af36802350031d791d43d6edae04
OneTooMany <santosismael.115@gmail.com>
2023-06-02 21:24:20 -0700

    Mv projectArray into new file for storage

--- src/classes.js
+++ src/classes.js
@@ -10,42 +10,6 @@ export class project {
   }
 }
 
-export const projectArray = [
-  {
-    name: "My Project",
-    id: 1,
-    tasks: [
-      {
-        title: "Workout session",
-        description: "",
-        date: "2023-05-30",
-        type: "personal",
-        completed: false,
-      },
-      {
-        title: "Grocery trip",
-        description: "",
-        date: "2023-05-31",
-        type: "personal",
-        completed: false,
-      },
-    ],
-  },
-  {
-    name: "My Newer Project",
-    id: 2,
-    tasks: [
-      {
-        title: "Do Homework",
-        description: "Finish Learning ABC's",
-        date: "2023-05-31",
-        type: "personal",
-        completed: true,
-      },
-    ],
-  },
-];
-
 class task {
   constructor(title, description, date, type, completed) {
     this.title = title;
